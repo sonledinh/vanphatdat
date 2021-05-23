ffa6e554f0db65375c2022c1879f6a0b8f95b859
sonledinh <sonledinh@admicro.vn>
2021-05-23 13:56:01 +0700

    final

---
+++ contact.php
@@ -0,0 +1,77 @@
+<?php include 'header.php';?>
+<main>
+	<section class="box-banenr">
+		<div class="slide-banner">
+			<div class="item">
+				<div class="avarta"><img src="images/banner.jpg" class="img-fluid w-100" alt=""></div>
+				<div class="caption">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-7"></div>
+							<div class="col-md-5">
+								<div class="info">
+									<h4><img src="images/title-cap.png" class="img-fluid" alt=""></h4>
+									<h3>Chuyên cung cấp dịch vụ</h3>
+									<h2>IT Helpdesk</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<div class="avarta"><img src="images/banner.jpg" class="img-fluid w-100" alt=""></div>
+				<div class="caption">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-7"></div>
+							<div class="col-md-5">
+								<div class="info">
+									<h4><img src="images/title-cap.png" class="img-fluid" alt=""></h4>
+									<h3>Chuyên cung cấp dịch vụ</h3>
+									<h2>IT Helpdesk</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> 
+	</section>
+	<section class="box-contact">
+		<div class="container">
+			<div class="title text-center text-uppercase">
+				<h2><span>Liên hệ</span></h2>
+			</div>
+			<div class="content-contact">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="info-contact">
+							<ul>
+								<li><strong>Địa chỉ:</strong>169/13 Dương Văn Dương, Phường Tân Qúy, Quận Tân Phú, TP.HCM</li>
+								<li><strong>Liên hệ:</strong>028.626.553.10</li>
+								<li><strong>Fax:</strong>028.626.721.98</li>
+								<li><strong>Hotline:</strong>028.626.721.98</li>
+								<li><strong>E- Mail:</strong>nfo@vanphatdat.vn</li>
+								<li><strong>Website:</strong>https://vanphatdat.vn</li>
+							</ul>
+							<div class="desc">Xác định, chẩn đoán, và giải quyết vấn đề ở mức 1 đối với yêu cầu của người sử dụng máy tính cá nhân, phần mềm máy tính và phần cứng, mạng, Internet.Cung cấp hỗ trợ cài đặt, cấu hình cho người dùng cuối các phần cứng máy tính để bàn máy tính, phần mềm và thiết bị ngoại vi.</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="frm-contact">
+							<div class="item"><input type="text" class="inp_txt" placeholder="Họ và tên"></div>
+							<div class="item"><input type="text" class="inp_txt" placeholder="Email"></div>
+							<div class="item"><input type="text" class="inp_txt" placeholder="Sô điện thoại"></div>
+							<div class="item"><textarea name="" class="" placeholder="Nội dung"></textarea></div>
+							<div class="item"><input type="submit" placeholder="" value="Gửi"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+<?php include 'footer.php';?>     
+      
+      
---
+++ news.php
@@ -0,0 +1,85 @@
+<?php include 'header.php';?>
+<main>
+	<section class="box-banenr">
+		<div class="slide-banner">
+			<div class="item">
+				<div class="avarta"><img src="images/banner.jpg" class="img-fluid w-100" alt=""></div>
+				<div class="caption">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-7"></div>
+							<div class="col-md-5">
+								<div class="info">
+									<h4><img src="images/title-cap.png" class="img-fluid" alt=""></h4>
+									<h3>Chuyên cung cấp dịch vụ</h3>
+									<h2>IT Helpdesk</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<div class="avarta"><img src="images/banner.jpg" class="img-fluid w-100" alt=""></div>
+				<div class="caption">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-7"></div>
+							<div class="col-md-5">
+								<div class="info">
+									<h4><img src="images/title-cap.png" class="img-fluid" alt=""></h4>
+									<h3>Chuyên cung cấp dịch vụ</h3>
+									<h2>IT Helpdesk</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> 
+	</section>
+	<section class="box-service">
+		<div class="container">
+			<div class="title text-center text-uppercase">
+				<h2><span>Tin tức</span></h2>
+			</div>
+			<div class="list-service">
+				<div class="row">
+					<div class="col-md-4 col-sm-4">
+						<div class="item-service">
+							<div class="avarta"><a href=""><img src="images/service.jpg" class="img-fluid w-100" alt=""></a></div>
+							<div class="info">
+								<div class="date"><span><i class="fa fa-clock-o"></i>24</span>THÁNG 05 NĂM 2021</div>
+								<h3><a href="">Dịch vụ IT HELPDESK</a></h3>
+								<div class="desc">Xác định, chẩn đoán, và giải quyết vấn đề ở mức 1 đối với yêu cầu của người sử dụng máy tính cá nhân, phần mềm máy tính và phần cứng, mạng, Internet.</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4">
+						<div class="item-service">
+							<div class="avarta"><a href=""><img src="images/service.jpg" class="img-fluid w-100" alt=""></a></div>
+							<div class="info">
+								<div class="date"><span><i class="fa fa-clock-o"></i>24</span>THÁNG 05 NĂM 2021</div>
+								<h3><a href="">Dịch vụ IT HELPDESK</a></h3>
+								<div class="desc">Xác định, chẩn đoán, và giải quyết vấn đề ở mức 1 đối với yêu cầu của người sử dụng máy tính cá nhân, phần mềm máy tính và phần cứng, mạng, Internet.</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4">
+						<div class="item-service">
+							<div class="avarta"><a href=""><img src="images/service.jpg" class="img-fluid w-100" alt=""></a></div>
+							<div class="info">
+								<div class="date"><span><i class="fa fa-clock-o"></i>24</span>THÁNG 05 NĂM 2021</div>
+								<h3><a href="">Dịch vụ IT HELPDESK</a></h3>
+								<div class="desc">Xác định, chẩn đoán, và giải quyết vấn đề ở mức 1 đối với yêu cầu của người sử dụng máy tính cá nhân, phần mềm máy tính và phần cứng, mạng, Internet.</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+<?php include 'footer.php';?>     
+      
+      
